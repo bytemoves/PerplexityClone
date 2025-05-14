01268ec8fa63b590ade7213b262d0b97bfb8397c
bytemoves <morganmogean@gmail.com>
2025-05-14 17:11:00 +0300

    stream chat

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI,WebSocket
 from pydantic_models.chat_body import ChatBody
 from services.search_service import SearchService 
 from services.sort_source_service import SortSourceService
+from services.llm_service import LLMService
 
 
 app = FastAPI()
 
 search_service = SearchService()
 sort_source_service = SortSourceService()
+llm_service = LLMService()
 
-# @app.get("/")
-# def read_root():
-#     return {"message": "Hello, FastAPI!"}
+#chatwebsocket
+@app.websocket("ws/chat")
+async def websocket_chat_endpoint(websocket:WebSocket):
+    await websocket.accept()
+    try:
+        data = await websocket.receive_json()
+        query = data.get("query")
+        
+        
+        search_results = search_service.web_search(query)
+    
+        sorted_results = sort_source_service.sort_sources(query, search_results)
+        await websocket.send_json({
+            "type": "search_results",
+            "data": sorted_results
+        })
+        # print(sorted_results)
+        
+        for chunk in llm_service.generate_response(query,sorted_results):
+            await websocket.send_json({type:"content","data":chunk})
+        
+
+    except:
+        print("unexpected error occured")
+    finally:
+        await websocket.close()
 
 #chat
 @app.post("/chat")
@@ -22,8 +46,8 @@ def chat_endpoint(body: ChatBody):
     sorted_results = sort_source_service.sort_sources(body.query, search_results)
     # print(sorted_results)
     
-    
-    return body.query
+    response = llm_service.generate_response(body.query,sorted_results)
+    return response
 
 #seach web find appropriate source 
 #sort the source
--- services/llm_service.py
+++ services/llm_service.py
@@ -1,2 +1,40 @@
+import google.generativeai as genai
+
+from config import Settings
+
+settings = Settings()
+
 class LLMService:
-    def generate_response(s)
+    
+    def __init__(self):
+        genai.configure(api_key=settings.GEMINI_API_KEY)
+        self.model = genai.GemerativeModel("gemini-2.0-flash")
+    def generate_response(self,query: str,search_results: list[dict]):
+        
+        #firt source url
+        #second soircr url
+        #query:
+        #provide detailed response bsed on this
+        
+        context_text = "\n\n".join([\
+            # url : content for every source
+            f"source {i+1} {result['url']}: \n{result['content']}" # 
+            for i , result in enumerate (search_results)
+        ])
+        
+        full_prompt = f"""
+        
+        Context from web search:
+        {context_text}
+
+        Query: {query}
+
+        Please provide a comprehensive, detailed, well-cited accurate response using the above context do not exceed 100 words.
+        Think and reason deeply. Ensure it answers the query the user is asking. Do not use your knowledge until it is absolutely necessary
+        
+        """
+        
+        response = self.model.generate.content(full_prompt,stream=True)
+        
+        for chunk in response:
+            yield chunk.text
